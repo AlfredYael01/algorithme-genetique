5c29098d33c97f4ee94301523e3c8378b2294050
Luis <luis.arguelles-gonzalez@etu.iut-tlse3.fr>
2023-10-05 13:22:37 +0200

    - Affichage graphique du parcours le plus court (sans mutation)

--- Functions.py
+++ Functions.py
@@ -4,6 +4,26 @@ import matplotlib.pyplot as plt
 import math
 
 villes = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+villesStr = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+def faireListInt(listStr):
+
+    listInt = [[int(ville) for ville in parcours] for parcours in listStr]
+
+    return listInt
+def faireListStrings(individues):
+
+    individuesStr = []
+    invStr = ''
+
+    for individue in individues:
+
+        for l in individue:
+            invStr += str(l)
+
+        individuesStr.append(invStr)
+        invStr = ''
+
+    return individuesStr
 
  # Coordonnees X
 coordonneesX = np.random.rand(10)
@@ -16,7 +36,7 @@ coordonnees = [i for (i) in zip(coordonneesX, coordonneesY)]
 
 # Villes et coordonnees X, Y ensemble dans un dictionnaire
 coordonneesVilles = {ville: i for (ville, i) in zip(villes, coordonnees)}
-
+coordonnesVillesStr = {ville: i for (ville, i) in zip(villesStr, coordonnees)}
 individues = []
 distances = []
 
@@ -32,8 +52,14 @@ def creer_individues(villes):
     for n in range(0, 100):
 
         individues.append(random.sample(villes, k=len(villes)))
+        individues[n].append(individues[n][0])
 
-    return individues
+    #print("Individues:", individues)
+    #print("Nombre d'individues: ", len(individues))
+
+    individuesStr = faireListStrings(individues)
+
+    return individues, individuesStr
 
 
 def calculer_distance_entre_2_villes(ville1, ville2):
@@ -50,6 +76,9 @@ def calculer_distance_entre_2_villes(ville1, ville2):
 
 
 def calculer_distances(individues):
+
+    distances.clear()
+
     for individu in individues:
 
         distance = 0
@@ -74,6 +103,7 @@ def calculer_distances(individues):
 
         # On sauvegarde la distance du parcours de chaque individu dans une liste
         distances.append(distance)
+
 
     return distances
 
@@ -110,18 +140,31 @@ def croisement(dic50):
 
         # Cherche pour des doubles
         for f1 in fils1:
-
+            
             nb = fils1.count(f1)
+            index = fils1.index(f1)
+            
+            if index == 0 and nb == 2:
+                continue
 
-            if nb >= 2 and f1 not in doubles1:
-                doubles1.append(f1)
+            else:
+                
+                if nb >= 2 and f1 not in doubles1:
+                    doubles1.append(f1)
+
 
         for f2 in fils2:
+            
+            nb = fils2.count(f2)  
+            index = fils2.index(f2)
+            
+            if index == 0 and nb == 2:
+                continue
+            
+            else:
 
-            nb = fils2.count(f2)
-
-            if nb >= 2 and f2 not in doubles2:
-                doubles2.append(f2)
+                if nb >= 2 and f2 not in doubles2:
+                    doubles2.append(f2)
 
         # Eliminations des doubles
         if len(doubles1) > 0 and len(doubles2) > 0:
@@ -129,22 +172,61 @@ def croisement(dic50):
             fils1 = fils1.replace(doubles1[0], doubles2[0], 1)
             fils2 = fils2.replace(doubles2[0], doubles1[0], 1)
 
-            if len(doubles1) > 1 and len(doubles2) >= 1:
+            if len(doubles1) > 1 and len(doubles2) > 1:
                 fils1 = fils1.replace(doubles1[1], doubles2[1], 1)
                 fils2 = fils2.replace(doubles2[1], doubles1[1], 1)
+
+                if len(doubles1) > 2 and len(doubles2) > 2:
+                    fils1 = fils1.replace(doubles1[2], doubles2[2], 1)
+                    fils2 = fils2.replace(doubles2[2], doubles1[2], 1)
 
         generation1.append(fils1)
         generation1.append(fils2)
 
         count += 1
 
-    print(count)
-    print(generation1)
-    print(len(generation1))
+    generation1Int= faireListInt(generation1)
+    #print("Generation 1 string:", generation1)
+    #print("Generation 1 int: ", generation1Int)
+    #print("Nombres d'enfants generation 1: ", len(generation1Int))
 
-    return generation1
+    return generation1, generation1Int
+
+
+
+
+def plot(parcours):
+
+    plt.figure(figsize=(10, 10))
+    plt.plot(coordonneesX, coordonneesY, 'o-')
+    for i, city in enumerate(parcours):
+        plt.text(city[0], city[1], str(i), fontsize=12, ha='right')
+    plt.title('Le parcours le plus court')
+    plt.xlabel('x')
+    plt.ylabel('y')
+    plt.grid(True)
+    plt.show()
+
+def plot2(cheminPlusCourt, distanceOptimale):
+
+    #print("Coordonnées villes: ", coordonnesVillesStr)
+
+    x = []
+    y = []
+
+    for ville in cheminPlusCourt:
+        x.append(coordonnesVillesStr[ville][0])
+        y.append(coordonnesVillesStr[ville][1])
+
+    plt.plot(x, y, marker='o')
+
+    for i, city in enumerate(coordonnees):
+        plt.text(city[0], city[1], str(i), fontsize=12, ha='right')
+
 
 
 # Mutation
+
+
 
 
--- algoritme-genetique.py
+++ algoritme-genetique.py
@@ -1,39 +1,59 @@
 import random
+import matplotlib as plt
 import Functions as fc
 
+# Creation des villes et les 100 premiers individues
 villes = fc.creer_villes()
-individues = fc.creer_individues(villes)
+individues, individuesStr = fc.creer_individues(villes)
+
+# Calcul des dictances des 100 premiers parcours
 distances = fc.calculer_distances(individues)
+#print("Nombre de distances des premiers individues", len(distances))
 
-individuesStr = []
-invStr = ''
 
-for individue in individues:
 
-    for l in individue:
-        invStr += str(l)
+#individuesStr = fc.faireListStrings()
 
-    individuesStr.append(invStr)
-    invStr = ''
-
-#creation d'un dictionnaire comprehension ou la cle est l'individu et la valeur est la distance
+# Creation d'un dictionnaire comprehension ou la cle est l'individu et la valeur est la distance
 dictIndivDist = {individue: distance for (individue, distance) in zip(individuesStr, distances)}
 
-#on tri le 50dictionnaire par ordre croissant de distance
+# On tri le dictionnaire par ordre croissant de distance
 dictIndivDist = dict(sorted(dictIndivDist.items(), key=lambda item: item[1]))
+
+# On prend les 50 meilleurs individues (parcours)
+
 dic50 = fc.dictionnaire50(dictIndivDist)
 
+
 # On crée la premiere generation
-generation1 = fc.croisement(dic50)
+generation1, generation1Int = fc.croisement(dic50)
 
+# On calcule les distances de la premiere generation
+ListDistancesGen1 = fc.calculer_distances(generation1Int)
+#print(ListDistancesGen1)
+#print("Nombre de distances generation 1: ", len(ListDistancesGen1))
 
+distanceOptimale = min(ListDistancesGen1)
+print(distanceOptimale)
 
-#affichage des croisement de 2 individues
+index = ListDistancesGen1.index(distanceOptimale)
+print(index)
 
+cheminPlusCourt = generation1[index]
+print(cheminPlusCourt)
 
+#dictIndivDistGen1 = {individue: distance for (individue, distance) in ()}
 
+#print("Nombre d'instances dans le dictionnaire", len(dictIndivDistGen1))
 
+# Tri de la liste par ordre croisant
+# dictIndivDistGen1 = dict(sorted(dictIndivDistGen1.items(), key=lambda item: item[1]))
 
+# affichage graphique avec l'import matplotlib de la premiere distance du dictionnaire DictIndivdistancesGen1 dans la console
+
+# affichage des croisement de 2 individues
+
+fc.plot2(cheminPlusCourt, distanceOptimale)
 
 
 
